5b9311a062a61470454cc64e65ef5d86f1d31296
Silthreent <s.il3ntscript3r@gmail.com>
2017-09-18 21:47:08 -0400

    Starting commit It logs in

--- GlurrrBotDiscord2/Program.cs
+++ GlurrrBotDiscord2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,50 @@ namespace GlurrrBotDiscord2
 {
     class Program
     {
+        static DiscordClient discord;
+
         static void Main(string[] args)
         {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader("botcode.txt"))
+                {
+                    discord = new DiscordClient(new DiscordConfiguration
+                    {
+                        Token = sr.ReadLine(),
+                        TokenType = TokenType.Bot,
+                        UseInternalLogHandler = true,
+                        LogLevel = LogLevel.Debug
+                    });
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("Code file not found");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("wtf happens");
+                Console.WriteLine(e.Message);
+            }
+
+            discord.MessageCreated += async e =>
+            {
+                if(e.Message.Content.ToLower().StartsWith("ping"))
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+            };
+
+            await discord.ConnectAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
